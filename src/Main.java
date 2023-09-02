f955aba53a7ab2976388795d9aebb591c1fffa6d
Andrey Strebkov <as19772023as@gmail.com>
2023-09-02 15:40:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        StreamMain streamMain = new StreamMain();
+        streamMain.main();
+        System.out.println();
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> newList = new ArrayList<>();
+         for (int x : intList){
+             if (x > 0 && x%2 == 0) newList.add(x);
+         }
+        Collections.sort(newList);
+        System.out.println(newList);
+
+    }
+}
